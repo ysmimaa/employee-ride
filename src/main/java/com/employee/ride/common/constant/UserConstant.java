f85ad1189dceb4f508e84af73df49ec09f890d81
ysmimaa <youssef.smimaa@gmail.com>
2020-10-06 23:12:02 +0200

    Adding user entity

---
+++ src/main/java/com/employee/ride/common/constant/UserConstant.java
@@ -0,0 +1,7 @@
+package com.employee.ride.common.constant;
+
+public interface UserConstant {
+
+    String TABLE_NAME = "USER";
+
+}
---
+++ src/main/java/com/employee/ride/entity/ContractType.java
@@ -0,0 +1,7 @@
+package com.employee.ride.entity;
+
+public enum ContractType {
+    CDD,
+    CDI,
+    INTERIM
+}
