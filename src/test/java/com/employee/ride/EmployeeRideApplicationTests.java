f9171758ee08c953d7fe6aa8681254769c09d06c
ysmimaa <youssef.smimaa@gmail.com>
2020-10-06 13:15:55 +0200

    add gardle config

---
+++ src/test/java/com/employee/ride/EmployeeRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
