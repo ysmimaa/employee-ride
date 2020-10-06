169c9291509e5999924925ccbd651af0c3726a01
ysmimaa <youssef.smimaa@gmail.com>
2020-10-06 13:15:55 +0200

    add gardle config

---
+++ src/main/java/com/employee/ride/EmployeeRideApplication.java
@@ -0,0 +1,13 @@
+package com.employee.ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRideApplication.class, args);
+	}
+
+}
