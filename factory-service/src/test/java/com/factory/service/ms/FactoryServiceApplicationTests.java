362a64475198a5928d751d96f8f6761c7d3143c5
ysmimaa <youssef.smimaa@gmail.com>
2020-12-28 00:36:10 +0100

    [factory-service] init commit

---
+++ factory-service/src/test/java/com/factory/service/ms/FactoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.service.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
